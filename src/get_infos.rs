1c8dde88451cf57864988c91da3d4d71d2e65cdb
CrzyNk12 <demarcossalves@gmail.com>
2022-10-09 12:30:14 -0300

    Memory info is more precise now!

--- src/get_infos.rs
+++ src/get_infos.rs
@@ -9,6 +9,8 @@ use sys_info::{
 
 use std::process::Command;
 
+use std::cmp::Ordering;
+
 use sysinfo::{
     CpuExt,
     SystemExt,
@@ -21,8 +23,16 @@ pub fn all_infos(color: &str) -> String {
     let get_type = linux_os_release().unwrap().pretty_name.unwrap();
 
     let total_mem:f32 = (mem_info().unwrap().total/102400) as f32 / 10.0;
-    let free_mem:f32 = (mem_info().unwrap().free/102400) as f32 / 10.0;
+    let used_mem = (mem_info().unwrap().total - (mem_info().unwrap().free + mem_info().unwrap().buffers + mem_info().unwrap().cached)) / 1024;
 
+    let used_mem: String = match used_mem.cmp(&1000) {
+        Ordering::Equal | Ordering::Greater => {
+            format!("{}Gb", (used_mem/100) as f32 / 10.0)
+        },
+        Ordering::Less => {
+            format!("{}Mb", used_mem)
+        }
+    };
     let host = hostname().unwrap();
 
     let kernel = os_release().unwrap();
@@ -39,8 +49,8 @@ pub fn all_infos(color: &str) -> String {
 {color}HOSTNAME\x1b[0m -> {host}
   {color}KERNEL\x1b[0m -> {kernel}
   {color}UPTIME\x1b[0m -> {uptime}
-     {color}MEM\x1b[0m -> {free_mem}Gb/{total_mem}Gb (free/total)
      {color}CPU\x1b[0m -> {cpu}
+     {color}MEM\x1b[0m -> {used_mem}/{total_mem}Gb (used/total)
    {color}PROCS\x1b[0m -> {procs}
    {color}SHELL\x1b[0m -> {shell}
     ")
